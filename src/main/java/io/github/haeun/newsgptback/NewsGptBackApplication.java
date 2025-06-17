af49d091d79f911e813bee33a72ebb05eb7176a0
shin-no1 <shinhe524@gmail.com>
2025-06-17 13:12:48 +0900

    create: project

---
+++ src/main/java/io/github/haeun/newsgptback/NewsGptBackApplication.java
@@ -0,0 +1,13 @@
+package io.github.haeun.newsgptback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsGptBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsGptBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/haeun/newsgptback/NewsGptBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.haeun.newsgptback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsGptBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
