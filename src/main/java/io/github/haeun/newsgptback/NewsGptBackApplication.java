95e163358f7122d680fde7af3323cd00b0fe476f
shin-no1 <shinhe524@gmail.com>
2025-06-17 16:18:25 +0900

    fix: unused database

--- src/main/java/io/github/haeun/newsgptback/NewsGptBackApplication.java
+++ src/main/java/io/github/haeun/newsgptback/NewsGptBackApplication.java
@@ -3,7 +3,13 @@ package io.github.haeun.newsgptback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class
+        }
+)
 public class NewsGptBackApplication {
 
     public static void main(String[] args) {
