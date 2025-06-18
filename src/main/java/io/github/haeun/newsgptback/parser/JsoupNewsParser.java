a7d5d5129496e74a7a9d86b16cadae4410f34b68
shin-no1 <shinhe524@gmail.com>
2025-06-18 16:10:08 +0900

    fix: add title select

--- src/main/java/io/github/haeun/newsgptback/parser/JsoupNewsParser.java
+++ src/main/java/io/github/haeun/newsgptback/parser/JsoupNewsParser.java
@@ -13,12 +13,10 @@ import java.io.IOException;
 public class JsoupNewsParser {
     public NewsResponse parse(String url) {
         try {
-            if (!url.startsWith("https://n.news.naver.com/")) {
-                throw new RuntimeException("네이버 뉴스가 아닙니다.");
-            }
+            if (!url.startsWith("https://n.news.naver.com/")) throw new RuntimeException("네이버 뉴스가 아닙니다.");
 
             Document doc = Jsoup.connect(toPrintUrl(url)).get();
-            String title = doc.title();
+            String title = doc.select("#title_area").text();
             String content = doc.select("#dic_area").text();
             return new NewsResponse(title, content);
         } catch (IOException e) {
