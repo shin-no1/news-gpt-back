62f9a48a519f5e7c5d11c9a1b01c5b21a83e8417
shin-no1 <shinhe524@gmail.com>
2025-06-20 15:11:00 +0900

    feat: add database enctyptor

--- src/main/java/io/github/haeun/newsgptback/NewsGptBackApplication.java
+++ src/main/java/io/github/haeun/newsgptback/NewsGptBackApplication.java
@@ -1,15 +1,11 @@
 package io.github.haeun.newsgptback;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-        exclude = {
-                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-                org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class
-        }
-)
+@EnableEncryptableProperties
+@SpringBootApplication
 public class NewsGptBackApplication {
 
     public static void main(String[] args) {
