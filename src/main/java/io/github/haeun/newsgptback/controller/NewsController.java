fe18e488cd0c54fe4860d16dba5407c956b516da
shin-no1 <shinhe524@gmail.com>
2025-06-20 12:01:00 +0900

    docs: Swagger 적용

--- src/main/java/io/github/haeun/newsgptback/controller/NewsController.java
+++ src/main/java/io/github/haeun/newsgptback/controller/NewsController.java
@@ -3,6 +3,13 @@ package io.github.haeun.newsgptback.controller;
 import io.github.haeun.newsgptback.dto.NewsRequest;
 import io.github.haeun.newsgptback.dto.NewsResponse;
 import io.github.haeun.newsgptback.service.NewsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "뉴스 요약", description = "뉴스 기사 URL을 기반으로 GPT 요약을 수행하는 API")
 public class NewsController {
     private final NewsService newsService;
 
@@ -23,6 +31,12 @@ public class NewsController {
      * @param request 요약할 뉴스 기사 URL
      * @return 분석된 뉴스 결과 (제목, 요약, 주제, 키워드)
      */
+    @Operation(
+            summary = "뉴스 요약 분석",
+            description = "뉴스 기사 URL을 입력받아 제목, 요약, 키워드 등을 반환합니다."
+    )
+    @Parameters({@Parameter(name = "url", description = "URL", example = "https://n.news.naver.com/article/015/0005146248")})
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = NewsResponse.class)))
     @PostMapping("/analyze-url")
     public NewsResponse analyzeUrl(@RequestBody NewsRequest request) {
         NewsResponse newsResponse = newsService.getNewsResponse(request.getUrl());
--- src/main/java/io/github/haeun/newsgptback/dto/NewsRequest.java
+++ src/main/java/io/github/haeun/newsgptback/dto/NewsRequest.java
@@ -1,10 +1,16 @@
 package io.github.haeun.newsgptback.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class NewsRequest {
+    @Schema(description = "분석할 뉴스 기사 URL", example = "https://n.news.naver.com/article/015/0005146248")
     private String url;
+
+    public NewsRequest(String url) {
+        this.url = url;
+    }
 }
